1de0696e21b7d343d613fea386f9b7f1fbf8e62b
AvantaR <avantar@gmail.com>
2020-02-11 14:10:02 +0100

    Update paponacciNumbers.php

--- paponacciNumbers.php
+++ paponacciNumbers.php
@@ -1,15 +1,16 @@
 <?php
 
 /**
-* @return array
-*/
-function paponacciNumbers(){
-	$paponacciNumbers = [];
-	for ($i = 0; $i <= 2137; $i++) {
-      if (preg_match('/[2137]{4}/', $i)) {
-          $paponacciNumbers[] = $i;
-      }
-  }
-  
-  return [];
+ * @return array
+ */
+function paponacciNumbers()
+{
+    $paponacciNumbers = [];
+    for ($i = 0; $i <= 2137; $i++) {
+        if (preg_match('/[2137]{4}/', $i)) {
+            $paponacciNumbers[] = $i;
+        }
+    }
+
+    return [];
 }
