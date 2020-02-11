91c5680619e4ec07ba7a328e2d47b61d7bb1df4d
AvantaR <avantar@gmail.com>
2020-02-11 14:09:21 +0100

    Create paponacciNumbers.php

---
+++ paponacciNumbers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* @return array
+*/
+function paponacciNumbers(){
+	$paponacciNumbers = [];
+	for ($i = 0; $i <= 2137; $i++) {
+      if (preg_match('/[2137]{4}/', $i)) {
+          $paponacciNumbers[] = $i;
+      }
+  }
+  
+  return [];
+}
