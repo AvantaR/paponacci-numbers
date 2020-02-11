2a66c7fd37043f1704c2498688ddf202e7dca034
AvantaR <avantar@gmail.com>
2020-02-11 15:00:43 +0100

    Update paponacciNumbers.php

--- paponacciNumbers.php
+++ paponacciNumbers.php
@@ -1,16 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * @return array
+ * @param int $highestNumber
+ * @return Generator
  */
-function paponacciNumbers()
+function paponacciNumbers(int $highestNumber = 2137): Generator
 {
-    $paponacciNumbers = [];
-    for ($i = 0; $i <= 2137; $i++) {
+    for ($i = 0; $i <= $highestNumber; $i++) {
         if (!preg_match('/[045689]{1}/', $i)) {
-            $paponacciNumbers[] = $i;
+            yield $i;
         }
     }
-
-    return $paponacciNumbers;
 }
